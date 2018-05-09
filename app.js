db9932c7aa0ca05e0a8f9319b00a23a59432d530
Scott Tracy <scott.tracy.st@gmail.com>
2018-05-09 11:22:02 -0500

    added if statement

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let hours = prompt("Please enter number of hours","(hours)")
+let pay = prompt("Please enter wage","(wage)")
+
+let product = pay * hours;
+
+console.log(product);
+if ((isNaN(hours)) || (isNaN(pay))) { 
+	alert( "I need numbers to work!");}
+else if (product > 1000)	{alert("$" + product + " " + "is a lot of money!");}
+else {
+alert("Your total pay is" + " " + "$" + (product));}
