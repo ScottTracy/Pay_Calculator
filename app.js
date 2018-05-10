3ebefd3f48fe136498b138c1200e050ee8e45da7
Scott Tracy <scott.tracy.st@gmail.com>
2018-05-10 07:17:40 -0500

    added overtime calc

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 let hours = prompt("Please enter number of hours","(hours)")
 let pay = prompt("Please enter wage","(wage)")
+let product
 
-let product = pay * hours;
+product = pay * hours;
 
 console.log(product);
 if ((isNaN(hours)) || (isNaN(pay))) { 
 	alert( "I need numbers to work!");}
-else if (product > 1000)	{alert("$" + product + " " + "is a lot of money!");}
+else if (hours > 40) { product = pay * 40 + (hours - 40)*(pay * 1.5);
+	alert("Wow, you worked too much to make just" + "$" + product)}
+else if (product > 1000)	{alert("$" + product + " " + "is a lot of money!");}	
 else {
-alert("Your total pay is" + " " + "$" + (product));}
+  alert("Your total pay is" + " " + "$" + (product));}
